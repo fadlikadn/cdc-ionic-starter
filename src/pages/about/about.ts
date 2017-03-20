47ea801b327d73c7596cc9f23c84d96d0c7b5d8e
Fadlika Dita Nurjanto <fadlika@kidsoft.com.au>
2017-03-20 13:29:10 +0700

    Add ionic sliding

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, Slides } from 'ionic-angular';
 
 /*
   Generated class for the About page.
@@ -12,12 +12,37 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  @ViewChild('picSlider') viewer: Slides;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // console.dir(navParams.data);
     // console.log(navParams.data.id);
     // console.log(navParams.data.name);
     // console.log(navParams.data.description);
+  }
+
+  ngAfterViewInit() {
+    this.viewer.pager = true;
+    this.viewer.speed = 750;
+  }
+
+  // slideOpts = {
+  //   pager: true,
+  //   speed: 750
+  // };
+
+  onSlideMoved() {
+    let currIndex = this.viewer.getActiveIndex();
+    let numSlides = this.viewer.length();
+    let firstSlide = this.viewer.isBeginning();
+    let lastSlide = this.viewer.isEnd();
+
+    console.log(`Current index is, ${currIndex}`);
+    if (firstSlide) {
+      console.log(`This is the first slide of ${numSlides} slides`);
+    } else if (lastSlide) {
+      console.log(`This is the last slide of ${numSlides} slides`);
+    }
   }
 
   ionViewDidLoad() {
